3dc6fe367b2c274a7d1cebc51bcb0c714b8c93db
Manya2507 <manyasingla2507@gmail.com>
2026-07-30 11:36:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,126 +99,120 @@ def predict():
 
 
             "Age":
-            float(data.get("Age",0)),
-
+            "Age": float(data.get("Age") or 0),
 
             "Category":
-            data.get("Category","General"),
+            "Category": category_map.get(data.get("Category"), 4),
 
 
 
             "Family_Income":
-            float(data.get("Family_Income",0)),
+            "Family_Income": float(data.get("Family_Income") or 0),
 
 
 
             "Class10_%":
-            float(data.get("Class10_%",0)),
+            "Class10_%": float(data.get("Class10_%") or 0),
 
 
 
             "Class12_%":
-            float(data.get("Class12_%",0)),
+            "Class12_%": float(data.get("Class12_%") or 0),
 
 
 
             "PCM_%":
-            float(data.get("PCM_%",0)),
+            "PCM_%": float(data.get("PCM_%") or 0),
 
 
 
             "Entrance_Exam":
-            data.get("Entrance_Exam",""),
+            "Entrance_Exam": exam_map.get(data.get("Entrance_Exam"), 0),
 
 
 
             "JEE_Percentile":
-            float(data.get("JEE_Percentile",0)),
+            "JEE_Percentile": float(data.get("JEE_Percentile") or 0),
 
 
 
             "JEE_Rank":
-            float(data.get("JEE_Rank",0)),
+            "JEE_Rank": float(data.get("JEE_Rank") or 0),
 
 
 
             "CUET_Score":
-            float(data.get("CUET_Score",0)),
-
+            "CUET_Score": float(data.get("CUET_Score") or 0),
 
 
             "Preferred_Branch":
-            data.get("Preferred_Branch",""),
-
+            "Preferred_Branch": branch_map.get(data.get("Preferred_Branch"), 0),
 
 
             "Preferred_College":
-            data.get("Preferred_College",""),
+             "Preferred_College": college_map.get(data.get("Preferred_College"), 0),
 
 
 
             "College_Type":
-            data.get("College_Type",""),
+             "College_Type": college_type_map.get(data.get("College_Type"), 0),
 
 
 
             "NIRF_Rank":
-            float(data.get("NIRF_Rank",0)),
-
+            "NIRF_Rank": float(data.get("NIRF_Rank") or 0),
 
 
             "College_Tier":
-            float(data.get("College_Tier",0)),
+            "College_Tier": tier_map.get(data.get("College_Tier"), 0),
 
 
 
             "Branch_Cutoff_Rank":
-            float(data.get("Branch_Cutoff_Rank",0)),
+            "Branch_Cutoff_Rank": float(data.get("Branch_Cutoff_Rank") or 0),
 
 
 
             "Available_Seats":
-            float(data.get("Available_Seats",0)),
+            "Available_Seats": float(data.get("Available_Seats") or 0),
 
 
 
             "Reservation_Quota":
-            data.get("Reservation_Quota",""),
-
+             "Reservation_Quota": quota_map.get(data.get("Reservation_Quota"), 4),
 
 
             "Documents_Verified":
-            data.get("Documents_Verified","No"),
+             "Documents_Verified": documents_map.get(data.get("Documents_Verified"), 0),
 
 
 
             "Interview_Score":
-            float(data.get("Interview_Score",0)),
+            "Interview_Score": float(data.get("Interview_Score") or 0),
 
 
 
             "Communication_Score":
-            float(data.get("Communication_Score",0)),
+            "Communication_Score": float(data.get("Communication_Score") or 0),
 
 
 
             "Aptitude_Score":
-            float(data.get("Aptitude_Score",0)),
+            "Aptitude_Score": float(data.get("Aptitude_Score") or 0),
 
 
 
             "Scholarship_Applied":
-            data.get("Scholarship_Applied","No"),
+            "Admission_Probability": float(data.get("Admission_Probability") or 0),
 
 
 
             "Scholarship_Eligibility":
-            data.get("Scholarship_Eligibility","No"),
-
+            "Scholarship_Eligibility": scholarship_eligibility_map.get(data.get("Scholarship_Eligibility"), 0),
 
 
             "Hostel_Required":
-            data.get("Hostel_Required","No"),
+             "Hostel_Required": hostel_map.get(data.get("Hostel_Required"), 0),
 
 
 
@@ -228,7 +222,7 @@ def predict():
 
 
             "Tuition_Fee":
-            float(data.get("Tuition_Fee",0))
+            "Tuition_Fee": float(data.get("Tuition_Fee") or 0),
 
 
         }])
